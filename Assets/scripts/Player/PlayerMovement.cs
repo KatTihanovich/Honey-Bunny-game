f48e47397b4e7c086c43b98dd4ccc36c43d54957
Artsiom <artsiom.chyslou@student.ehu.lt>
2024-11-27 14:38:53 +0200

    Emergency commit

--- Assets/scripts/Player/PlayerMovement.cs
+++ Assets/scripts/Player/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using Spine.Unity;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,26 +9,64 @@ public class PlayerMovement : MonoBehaviour
 
     private bool grounded;  //Temp fix
     private Rigidbody2D body; 
-    private Animator anim;
     private BoxCollider2D boxCollider;
 
+    public bool isOnPlatform;
+    public Rigidbody2D platformRb;
+    public SkeletonAnimation skeletonAnimation;
+    public AnimationReferenceAsset idle, walking;
+    public string currentState;
+    public string currentAnimation;
+
+
+    private void Start()
+    {
+        {
+            currentState = "Idle";
+            SetCharacterState(currentState);
+        }
+    }
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
     }
     private void Update()
     {
+
         float horizontalInput = Input.GetAxis("Horizontal");
         body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
 
         if (horizontalInput > 0.01f)
-            transform.localScale = Vector3.one;
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); // Сохраняем текущий Y и Z
         else if (horizontalInput < -0.01f)
-            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z); // Сохраняем текущий Y и Z
+
+
+
+        if (horizontalInput !=0)
+        {
+            SetCharacterState ("Walking");
+        }
+        else
+        {
+            SetCharacterState("Idle");
+        }
 
         if (Input.GetKey(KeyCode.Space) && grounded)
             Jump();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isOnPlatform)
+        {
+            body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed + platformRb.linearVelocity.x, body.linearVelocity.y);
+        }
+        else
+        {
+           body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
+        }
     }
 
     private void Jump()
@@ -52,5 +91,25 @@ public class PlayerMovement : MonoBehaviour
     //    RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
     //    return raycastHit.collider != null;
     //}
+    public void SetAnimation(AnimationReferenceAsset animation, bool loop, float timescale)
+    {
+        if (animation.name.Equals(currentAnimation))
+        {
+            return;
+        }
+        skeletonAnimation.state.SetAnimation(0, animation, loop).TimeScale = timescale;
+        currentAnimation = animation.name;
+    }
 
+    public void SetCharacterState(string state)
+    {
+        if(state.Equals("Idle"))
+        {
+            SetAnimation(idle, true, 1f);
+        }
+        else if (state.Equals("Walking"))
+        {
+            SetAnimation(walking, true, 1f);
+        }
+    }
 }

1e0451cae6e1b81fed633c7582d19c918234f73a
alenatsibets <alena.tsibets@gmail.com>
2025-01-28 02:13:03 +0200

    added tutorial

--- Assets/scripts/interface/MainMenu.cs
+++ Assets/scripts/interface/MainMenu.cs
@@ -8,4 +8,9 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(1);
     }
 
+    public void PlayTutorial()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadSceneAsync(2);
+    }
 }

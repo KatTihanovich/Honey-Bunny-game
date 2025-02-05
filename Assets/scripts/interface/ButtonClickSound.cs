e81112cc74d4e1800e51322a461f72ddf222201c
alenatsibets <alena.tsibets@gmail.com>
2025-02-05 14:41:47 +0100

    increased buttons scaling

--- Assets/scripts/interface/ButtonClickSound.cs
+++ Assets/scripts/interface/ButtonClickSound.cs
@@ -1,17 +1,29 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
-public class ButtonClickSound : MonoBehaviour
+public class UIButtonSound : MonoBehaviour
 {
     [Header("Audio Settings")]
     [SerializeField] private AudioClip buttonSound;
     [SerializeField] private float volume = 1.0f;
+    [SerializeField] private AudioMixerGroup audioMixerGroup; 
 
     public void PlayClickSound()
     {
-        if (buttonSound != null)
+        if (buttonSound != null && audioMixerGroup != null)
         {
-            AudioSource.PlayClipAtPoint(buttonSound, transform.position, volume);
+            GameObject tempAudio = new GameObject("TempButtonSound");
+            AudioSource audioSource = tempAudio.AddComponent<AudioSource>();
+
+            audioSource.outputAudioMixerGroup = audioMixerGroup;
+            audioSource.clip = buttonSound;
+            audioSource.volume = volume;
+            audioSource.Play();
+
+            Destroy(tempAudio, buttonSound.length);
         }
     }
+
+
 }

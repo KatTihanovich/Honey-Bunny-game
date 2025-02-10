b107087fa822afe2445c205c2840ddfdb159d64a
KatTihanovich <kataviridi@gmail.com>
2025-02-10 21:58:06 +0200

    Fixed sound on flower puzzle

--- Assets/scripts/Puzzle/FlowerPuzzle/OtherFlowerInteraction.cs
+++ Assets/scripts/Puzzle/FlowerPuzzle/OtherFlowerInteraction.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using System.Collections;
 
 public class OtherFlowerInteraction : MonoBehaviour
 {
-    [SerializeField] private AudioClip otherFlowerMelody; 
-    [SerializeField] private AudioMixerGroup otherFlowerAudioGroup; 
+    [SerializeField] private AudioClip otherFlowerMelody;
+    [SerializeField] private AudioMixerGroup otherFlowerAudioGroup;
+    [SerializeField] private float fadeDuration = 1.0f; 
 
     private AudioSource otherFlowerAudioSource;
+    private Coroutine fadeCoroutine;
 
     private void Start()
     {
@@ -15,13 +18,18 @@ public class OtherFlowerInteraction : MonoBehaviour
         otherFlowerAudioSource.outputAudioMixerGroup = otherFlowerAudioGroup;
         otherFlowerAudioSource.loop = true;
         otherFlowerAudioSource.playOnAwake = false;
+        otherFlowerAudioSource.volume = 0f; 
     }
 
     private void OnBecameVisible()
     {
-        if (otherFlowerMelody != null && !otherFlowerAudioSource.isPlaying)
+        if (otherFlowerMelody != null)
         {
-            otherFlowerAudioSource.Play();
+            if (fadeCoroutine != null)
+            {
+                StopCoroutine(fadeCoroutine);
+            }
+            fadeCoroutine = StartCoroutine(FadeIn());
         }
     }
 
@@ -29,7 +37,49 @@ public class OtherFlowerInteraction : MonoBehaviour
     {
         if (otherFlowerAudioSource.isPlaying)
         {
-            otherFlowerAudioSource.Pause(); 
+            if (fadeCoroutine != null)
+            {
+                StopCoroutine(fadeCoroutine);
+            }
+            fadeCoroutine = StartCoroutine(FadeOut());
         }
     }
+
+    private IEnumerator FadeIn()
+    {
+        if (!otherFlowerAudioSource.isPlaying)
+        {
+            otherFlowerAudioSource.Play();
+        }
+
+        float startVolume = otherFlowerAudioSource.volume;
+        float targetVolume = 1f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            otherFlowerAudioSource.volume = Mathf.Lerp(startVolume, targetVolume, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        otherFlowerAudioSource.volume = targetVolume;
+    }
+
+    private IEnumerator FadeOut()
+    {
+        float startVolume = otherFlowerAudioSource.volume;
+        float targetVolume = 0f;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            otherFlowerAudioSource.volume = Mathf.Lerp(startVolume, targetVolume, elapsedTime / fadeDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        otherFlowerAudioSource.volume = targetVolume;
+        otherFlowerAudioSource.Pause(); 
+}
 }

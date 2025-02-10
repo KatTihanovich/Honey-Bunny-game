fb5bed5bdf291cba8bac0b290d55a367c20a15f8
alenatsibets <alena.tsibets@gmail.com>
2025-02-10 04:01:15 +0200

    forest new ui

--- Assets/scripts/interface/PauseMenu.cs
+++ Assets/scripts/interface/PauseMenu.cs
@@ -1,37 +1,59 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class PauseMenu : MonoBehaviour
 {
+    [Header("UI Elements")]
     public GameObject pauseMenuUI;
-    public Image image; 
-    public Sprite defaultSprite; 
+    public List<GameObject> menusToDisable = new List<GameObject>(); 
+    public Image image;
+    public Sprite defaultSprite;
 
-    void Update()
+    public void Resume()
     {
-        
-    }
-
-    public void Resume(){
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
     }
 
-    public void ResumeWithButton(){
+    public void ResumeWithButton()
+    {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        image.sprite = defaultSprite;
+        if (image != null)
+        {
+            image.sprite = defaultSprite;
+        }
     }
 
-    public void Pause(){
+    public void Pause()
+    {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
     }
 
-    public void LoadMenu(){
+    public void PauseWithDisabling()
+    {
+        pauseMenuUI.SetActive(true);
+        
+        foreach (GameObject menu in menusToDisable)
+        {
+            if (menu != null)
+            {
+                menu.SetActive(false);
+            }
+        }
+
+        Time.timeScale = 0f;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
         SceneManager.LoadSceneAsync(0);
     }
+
     public void Restart()
     {
         Time.timeScale = 1f;
--- Assets/scripts/interface/SelectLevelScript.cs
+++ Assets/scripts/interface/SelectLevelScript.cs
@@ -6,18 +6,15 @@ public class SelectLevelScript : MonoBehaviour
 {
     public GameObject panel;  
     public GameObject ui_panel; 
-    public Button select_button; 
 
     void Start()
     {
         panel.SetActive(false);
-        select_button.interactable = true;
     }
 
     public void TogglePanel()
     {
         panel.SetActive(true);
-        select_button.interactable = false;
         if (ui_panel != null)
         {
             ui_panel.SetActive(false);

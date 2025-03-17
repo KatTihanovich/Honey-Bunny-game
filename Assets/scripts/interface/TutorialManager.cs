6744808b6e5ebdd66bb33215bb8aaef450b9b00a
alenatsibets <alena.tsibets@gmail.com>
2025-03-17 23:10:07 +0200

    boss, tutorial pc ui

--- Assets/scripts/interface/TutorialManager.cs
+++ Assets/scripts/interface/TutorialManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class TutorialManager : MonoBehaviour
 {
     [Header("Tutorial UI Elements")]
     public GameObject tutorialCanvas; 
     public GameObject[] tutorialPanels; 
+    public Button[] selectableButtons; // Buttons to select for each panel
     private int currentPanelIndex = 0; 
 
     private void Start()
@@ -15,15 +17,18 @@ public class TutorialManager : MonoBehaviour
 
     private void ShowPanel(int index)
     {
+        // Deactivate all panels
         foreach (var panel in tutorialPanels)
         {
             panel.SetActive(false);
         }
 
+        // Activate target panel and select its button
         if (index >= 0 && index < tutorialPanels.Length)
         {
             tutorialPanels[index].SetActive(true);
             currentPanelIndex = index;
+            SelectButton(index);
         }
         else
         {
@@ -31,10 +36,19 @@ public class TutorialManager : MonoBehaviour
         }
     }
 
+    private void SelectButton(int index)
+    {
+        if (index >= 0 && index < selectableButtons.Length && selectableButtons[index] != null)
+        {
+            EventSystem.current.SetSelectedGameObject(selectableButtons[index].gameObject);
+        }
+    }
+
     public void SkipTutorial()
     {
         tutorialCanvas.SetActive(false);
     }
+
     public void NextPanel()
     {
         ShowPanel(currentPanelIndex + 1);

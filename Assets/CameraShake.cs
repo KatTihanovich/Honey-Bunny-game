7aa3e2d081fb376fea682af134bdb84b58473f72
alenatsibets <alena.tsibets@gmail.com>
2025-05-06 09:56:25 +0200

    camera shake with following fixed

--- Assets/CameraShake.cs
+++ Assets/CameraShake.cs
@@ -3,39 +3,15 @@ using UnityEngine;
 public class CameraShake : MonoBehaviour
 {
     public float intensity = 0.1f;
-    public float speed = 1.0f;
-
-    private Vector3 _originalPosition;
-    private bool _isShaking;
-
-    void Start()
-    {
-        _originalPosition = transform.localPosition;
-    }
-
-    void Update()
-    {
-        if (_isShaking)
-        {
-            transform.localPosition = _originalPosition + Random.insideUnitSphere * intensity;
-        }
-    }
+    public bool IsShaking { get; private set; }
 
     public void StartShaking()
     {
-        if (!_isShaking)
-        {
-            _originalPosition = transform.localPosition;
-            _isShaking = true;
-        }
+        IsShaking = true;
     }
 
     public void StopShaking()
     {
-        if (_isShaking)
-        {
-            _isShaking = false;
-            transform.localPosition = _originalPosition;
-        }
+        IsShaking = false;
     }
 }

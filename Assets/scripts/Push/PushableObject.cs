972c2309fb02167afaca073f1d7d37d46bce8698
alenatsibets <alena.tsibets@gmail.com>
2025-05-13 10:41:37 +0300

    stone, platform sounds

--- Assets/scripts/Push/PushableObject.cs
+++ Assets/scripts/Push/PushableObject.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using Game.Audio;
+
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PushableObject : MonoBehaviour
@@ -9,6 +11,14 @@ public class PushableObject : MonoBehaviour
 
     private bool _beingPushed;
     private float _pushDir;
+    private bool _wasGrounded;
+    private bool _wasFalling;
+    private ISoundManager _soundManager;
+
+    [Header("Ground Check")]
+    [SerializeField] private Transform _groundCheckPoint;
+    [SerializeField] private float _groundCheckRadius = 0.1f;
+    [SerializeField] private LayerMask _groundLayer;
 
     private void Awake()
     {
@@ -16,16 +26,17 @@ public class PushableObject : MonoBehaviour
         _rb.freezeRotation = true;
    
         _rb.linearDamping = 5f;
+        _soundManager = SoundManagerNew.Instance;
     }
 
     private void FixedUpdate()
     {
         if (_beingPushed)
         {
-
             Vector2 targetPos = _rb.position + Vector2.right * _pushDir * _pushSpeed * Time.fixedDeltaTime;
             _rb.MovePosition(targetPos);
         }
+        HandleFallingSound();
     }
 
 
@@ -38,5 +49,24 @@ public class PushableObject : MonoBehaviour
     public void StopPushing()
     {
         _beingPushed = false;
+    }
+
+    private void HandleFallingSound()
+    {
+        bool isGrounded = Physics2D.OverlapCircle(_groundCheckPoint.position, _groundCheckRadius, _groundLayer);
+        bool isFalling = _rb.linearVelocity.y < -0.1f;
+
+        if (!_wasFalling && isFalling && !_wasGrounded)
+        {
+            _wasFalling = true;
+        }
+
+        if (_wasFalling && isGrounded)
+        {
+            _wasFalling = false;
+            _soundManager.PlaySound("StoneFall");
+        }
+
+        _wasGrounded = isGrounded;
     }
 }

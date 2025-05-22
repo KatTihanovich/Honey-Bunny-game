3ca4daef37f832325468b878663e30cfa141ef1a
alenatsibets <alena.tsibets@gmail.com>
2025-05-22 22:24:33 +0200

    loading screen

--- Assets/scripts/Level/LevelLoader.cs
+++ Assets/scripts/Level/LevelLoader.cs
@@ -1,16 +1,33 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace Level
 {
     public class LevelLoader : MonoBehaviour
     {
         public int nextLevelIndex;
+        public GameObject LoadingScreen;
+        public Image LoadingBarFill;
 
         public void NextLevel()
         {
             Time.timeScale = 1f;
-            SceneManager.LoadSceneAsync(nextLevelIndex);
+            StartCoroutine(LoadSceneAsync());
+        }
+
+        IEnumerator LoadSceneAsync()
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(nextLevelIndex);
+            LoadingScreen.SetActive(true);
+
+            while (!operation.isDone)
+            {
+                float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+                LoadingBarFill.fillAmount = progressValue;
+                yield return null;
+            }
         }
     }
 }

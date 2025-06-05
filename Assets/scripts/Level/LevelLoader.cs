b72020b1a50076d11a28ee3b02d83c8ec7a0a2d0
alenatsibets <alena.tsibets@gmail.com>
2025-06-05 17:13:08 +0300

    fixes

--- Assets/scripts/Level/LevelLoader.cs
+++ Assets/scripts/Level/LevelLoader.cs
@@ -13,7 +13,15 @@ namespace Level
         public void NextLevel(int scene_id)
         {
             Time.timeScale = 1f;
-            StartCoroutine(LoadSceneAsync(scene_id));
+            if (LoadingScreen != null && LoadingBarFill != null)
+            {
+                StartCoroutine(LoadSceneAsync(scene_id));
+            }
+            else
+            {
+                AsyncOperation operation = SceneManager.LoadSceneAsync(scene_id);
+            }
+            
         }
 
         IEnumerator LoadSceneAsync(int scene_id)
